471d6a488c752b37bceeb02deec9c8b7fde54111
Jeffrey Li <jeffreyli8000@gmail.com>
2023-01-03 19:59:32 -0500

    aaron made me do extra work

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from './components/Footer.jsx';
 import Home from './components/Home/Home.jsx';
 import About from './components/About.jsx';
+import Terms from './components/Terms.jsx';
 
 const App = () => {
   return (
     <div>
       {/* <Home/> */}
-      <About/>
+      {/* <About/> */}
+      <Terms/>
     </div>
   )
 }
