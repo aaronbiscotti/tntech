2071f5526347374227aadfd6c3144fb482d1ce93
aaronbiscotti <thesoupmanguy@gmail.com>
2023-01-16 20:23:22 -0500

    sdfw

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,13 +7,13 @@ export function About() {
         <section
             id="pricing"
             aria-labelledby="pricing-title"
-            className="scroll-mt-14 pt-16 pb-8 sm:scroll-mt-32 sm:pt-20 sm:pb-10 lg:pt-32 lg:pb-16"
+            className="relative scroll-mt-14 pt-16 pb-8 sm:scroll-mt-32 sm:pt-20 sm:pb-10 lg:pt-32 lg:pb-16"
         >
             <Container>
                 <p className="mt-8 uppercase font-title text-5xl text-main sm:text-6xl">
                     Assembled by gamers, for gamers.
                 </p>
-                <p className="mt-4 text-lg tracking-tight text-main">
+                <p className="mt-4 text-lg max-w-3xl tracking-tight text-main">
                     Welcome to TNTech, where gaming is our passion and building top-of-the-line gaming computers is our expertise. Our team know firsthand what it takes to deliver the best gaming experience- from selecting the most powerful processors and graphics cards, to choosing the perfect components for optimal cooling and speed, we’ve got you covered for your budget.
                 </p>
             </Container>
