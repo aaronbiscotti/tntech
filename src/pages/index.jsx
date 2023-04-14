fcc4ec4c8d638712f7c6d38892f7eb64efdedef7
aaronbiscotti <thesoupmanguy@gmail.com>
2023-04-14 18:42:16 -0400

    love

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,8 +27,6 @@ export default function Home({ products }) {
           name="description"
           content="Unleash the full potential of your gaming with our intuitive building services. Complexity becomes a thing of the past, as we've distilled all the mission-critical features for a seamless gaming experience."
         />
-        <link rel="preload" href="/fonts/anton.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/hankengrotesk.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
       </Head>
       <Header />
       <main>
